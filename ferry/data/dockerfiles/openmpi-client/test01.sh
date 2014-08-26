f67a47bc74fd26cb13db51c85304d8d6c5bd55bf
James Horey <jlh@opencore.io>
2014-08-26 16:31:47 +0000

    rm yarn json

--- ferry/data/dockerfiles/openmpi-client/test01.sh
+++ ferry/data/dockerfiles/openmpi-client/test01.sh
@@ -7,9 +7,10 @@ GREEN='\e[0;32m'
 NC='\e[0m'
 
 # Commands
-MKDIRS='mkdir /service/data/binaries'
+MKBINS='mkdir /service/data/binaries'
+MKOUT='mkdir /service/data/outputs'
 COMPILE='mpic++ -W -Wall /service/examples/helloworld.cpp -o /service/data/binaries/helloworld.o'
-RUN='mpirun -np 4 --hostfile $MPI_CONF/hosts /service/data/binaries/helloworld.o'
+RUN='mpirun -np 4 --hostfile $MPI_CONF/hosts /service/data/binaries/helloworld.o >> /service/data/outputs/mpitest.out'
 
 function run_as_ferry {
     echo -e "${GREEN} ${2} ${NC}"
@@ -20,6 +21,7 @@ function run_as_ferry {
     fi
 }
 
-run_as_ferry "$MKDIRS" "Creating shared directory"
+run_as_ferry "$MKBINS" "Creating binary directory"
+run_as_ferry "$MKOUT" "Creating output directory"
 run_as_ferry "$COMPILE" "Compiling MPI application"
 run_as_ferry "$RUN" "Running MPI application"
