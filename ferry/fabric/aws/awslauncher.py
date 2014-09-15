17cf4fd07666bb8c2bef752dd6db7d2351341b18
James Horey <jlh@opencore.io>
2014-09-15 15:37:39 +0000

    aws security group plan

--- ferry/fabric/aws/awslauncher.py
+++ ferry/fabric/aws/awslauncher.py
@@ -37,11 +37,11 @@ class AWSLauncher(object):
 
         self._init_aws_stack(conf_file)
 
-    def _init_open_stack(self, conf_file):
+    def _init_aws_stack(self, conf_file):
         with open(conf_file, 'r') as f:
             args = yaml.load(f)
 
-            params = args['hp']['params']
+            params = args['aws']['params']
             self.default_dc = params['dc']
             self.default_zone = params['zone']
 
@@ -111,7 +111,7 @@ class AWSLauncher(object):
                  "cidr" : cidr }
         return plan, desc
 
-    def _create_security_group(self, group_name, network, ports):
+    def _create_security_group(self, group_name, ports, internal):
         """
         Create and assign a security group to the supplied server. 
         """
@@ -127,13 +127,23 @@ class AWSLauncher(object):
                                                                               "CidrIp": "0.0.0.0/0",
                                                                               "FromPort" : 22, 
                                                                               "ToPort" : 22 }]}}}
-        # Additional ports for the security group. 
+        # Ports for internal connections. 
         for p in ports:
             min_port = p[0]
             max_port = p[1]
             desc[group_name]["Properties"]["SecurityGroupIngress"].append({ "IpProtocol" : "tcp",
                                                                             "CidrIp": "0.0.0.0/0",
-                                                                            "FromPort" : max_port, 
+                                                                            "FromPort" : min_port, 
+                                                                            "ToPort" : max_port })
+
+        # Additional ports for the security group. These
+        # port values can only be accessed from within the same network. 
+        for p in internal:
+            min_port = p[0]
+            max_port = p[1]
+            desc[group_name]["Properties"]["SecurityGroupIngress"].append({ "IpProtocol" : "tcp",
+                                                                            "CidrIp": self.subnet["cidr"],
+                                                                            "FromPort" : min_port,
                                                                             "ToPort" : max_port })
         return desc
         
@@ -147,7 +157,7 @@ class AWSLauncher(object):
                                            
         return desc
 
-    def _create_server_init(self, instance_name, networks):
+    def _create_server_init(self, instance_name):
         """
         Create the server init process. These commands are run on the
         host after the host has booted up. 
@@ -164,18 +174,30 @@ class AWSLauncher(object):
                     "parted --script /dev/vdb mkpart primary xfs 0% 100%\n",
                     "mkfs.xfs /dev/vdb1\n", 
                     "mkdir /ferry/data\n",
+                    "mkdir /ferry/keys\n",
+                    "mkdir /ferry/containers\n",
                     "mount -o noatime /dev/vdb1 /ferry/data\n",
                     "export FERRY_SCRATCH=/ferry/data\n", 
                     "export FERRY_DIR=/ferry/master\n",
+                    "echo export FERRY_SCRATCH=/ferry/data >> /etc/profile\n", 
+                    "echo export FERRY_DIR=/ferry/master >> /etc/profile\n",
                     "export HOME=/root\n",
-                    "dhclient eth1\n", 
-                    "ferry server\n"
+                    "export USER=root\n",
+                    "mkdir /home/ferry/.ssh\n",
+                    "cp /home/%s/.ssh/authorized_keys /home/ferry/.ssh/\n" % self.default_user,
+                    "cp /home/%s/.ssh/authorized_keys /root/.ssh/\n" % self.default_user,
+                    "chown -R ferry:ferry /home/ferry/.ssh\n",
+                    "chown -R ferry:ferry /ferry/data\n",
+                    "chown -R ferry:ferry /ferry/keys\n",
+                    "chown -R ferry:ferry /ferry/containers\n",
+                    "ferry server -n\n",
+                    "sleep 3\n"
                   ]
               ]
           }}
         return user_data
 
-    def _create_instance(self, name, image, size, data_networks, sec_group):
+    def _create_instance(self, name, image, size, sec_group):
         """
         Create a new instance
         """
@@ -189,32 +211,20 @@ class AWSLauncher(object):
                           "ports" : [],
                           "volumes" : [] }}
 
-        # # Create a port for the data networks 
-        # port_descs = []
-        # for n in data_networks:
-        #     network = n[0]
-        #     subnet = n[1]
-        #     port_name = "ferry-port-%s-%s" % (name, network)
-        #     port_descs.append(self._create_port(port_name, network, sec_group, ref=False))
-
-        # # Combine all the port descriptions. 
-        # for d in port_descs:
-        #     plan = dict(plan.items() + d.items())
-
         # Now add the user script.
-        user_data = self._create_server_init(name, data_networks)
+        user_data = self._create_server_init(name)
         plan[name]["Properties"]["UserData"] = user_data
 
         return plan, desc
 
-    def _create_security_plan(self, cluster_uuid, ports):
+    def _create_security_plan(self, cluster_uuid, ports, internal, ctype):
         """
         Update the security group. 
         """
         sec_group_name = "ferry-sec-%s" % cluster_uuid
         plan = { "AWSTemplateFormatVersion" : "2010-09-09",
-                 "Description" : "Ferry generated Heat plan",
-                 "Resources" : self._create_security_group(sec_group_name, ports) }
+                 "Description" : "Ferry generated CloudFormation plan",
+                 "Resources" : self._create_security_group(sec_group_name, ports, internal) }
         desc = { sec_group_name : { "type" : "AWS::EC2::SecurityGroup" }}
         return plan, desc
 
@@ -238,7 +248,7 @@ class AWSLauncher(object):
         logging.info("create Heat network: " + str(desc))
         return plan, desc
 
-    def _create_instance_plan(self, cluster_uuid, num_instances, image, size, network, sec_group_name, ctype): 
+    def _create_instance_plan(self, cluster_uuid, num_instances, image, size, sec_group_name, ctype): 
         plan = { "AWSTemplateFormatVersion" : "2010-09-09",
                  "Description" : "Ferry generated Heat plan",
                  "Resources" : {},
@@ -248,7 +258,7 @@ class AWSLauncher(object):
         for i in range(0, num_instances):
             # Create the actual instances. 
             instance_name = "ferry-instance-%s-%s-%d" % (cluster_uuid, ctype, i)
-            instance_plan, instance_desc = self._create_instance(instance_name, image, size, self.manage_network, [network], sec_group_name)
+            instance_plan, instance_desc = self._create_instance(instance_name, image, size, self.manage_network, sec_group_name)
             plan["Resources"] = dict(plan["Resources"].items() + instance_plan.items())
             desc = dict(desc.items() + instance_desc.items())
 
@@ -260,14 +270,6 @@ class AWSLauncher(object):
         Launch the cluster plan.  
         """
         logging.info("launching heat plan: " + str(heat_plan))
-        
-        # Instruct Heat to create the stack, and wait 
-        # for it to complete. 
-
-        # Now find the physical IDs of all the resources. 
-
-        # Record the Stack ID in the description so that
-        # we can refer back to it later. 
 
     def _collect_resources(self, stack_id):
         """
@@ -291,7 +293,7 @@ class AWSLauncher(object):
         return self._launch_heat_plan("ferry-app-NET-%s" % cluster_uuid, stack_plan, stack_desc)
 
 
-    def create_app_stack(self, cluster_uuid, num_instances, network, security_group_ports, assign_floating_ip, ctype):
+    def _create_app_stack(self, cluster_uuid, num_instances, security_group_ports, internal_ports, assign_floating_ip, ctype):
         """
         Create an empty application stack. This includes the instances, 
         security groups, and floating IPs. 
@@ -299,43 +301,154 @@ class AWSLauncher(object):
 
         logging.info("creating security group for %s" % cluster_uuid)
         sec_group_plan, sec_group_desc = self._create_security_plan(cluster_uuid = cluster_uuid,
-                                                                      ports = security_group_ports)
+                                                                    ports = security_group_ports,
+                                                                    internal = internal_ports, 
+                                                                    ctype = ctype) 
 
-        logging.info("creating instances for %s" % cluster_uuid)
-        stack_plan, stack_desc = self._create_instance_plan(cluster_uuid = cluster_uuid, 
-                                                            num_instances = num_instances, 
-                                                            image = self.default_image,
-                                                            size = self.default_personality, 
-                                                            network = network,
-                                                            sec_group_name = sec_group_desc.keys()[0], 
-                                                            ctype = ctype)
+        return sec_group_plan
+        # logging.info("creating instances for %s" % cluster_uuid)
+        # stack_plan, stack_desc = self._create_instance_plan(cluster_uuid = cluster_uuid, 
+        #                                                     num_instances = num_instances, 
+        #                                                     image = self.default_image,
+        #                                                     size = self.default_personality, 
+        #                                                     sec_group_name = sec_group_desc.keys()[0], 
+        #                                                     ctype = ctype)
 
-        # See if we need to assign any floating IPs 
-        # for this stack. We need the references to the neutron
-        # port which is contained in the description. 
-        if assign_floating_ip:
-            logging.info("creating floating IPs for %s" % cluster_uuid)
+        # # See if we need to assign any floating IPs 
+        # # for this stack. We need the references to the neutron
+        # # port which is contained in the description. 
+        # if assign_floating_ip:
+        #     logging.info("creating floating IPs for %s" % cluster_uuid)
+        # else:
+        #     ip_plan = { "Resources" : {}}
+        #     ip_desc = {}
+
+        # # Now we need to combine all these plans and
+        # # launch the cluster. 
+        # stack_plan["Resources"] = dict(sec_group_plan["Resources"].items() + 
+        #                                ip_plan["Resources"].items() + 
+        #                                stack_plan["Resources"].items())
+        # stack_desc = dict(stack_desc.items() + 
+        #                   sec_group_desc.items() +
+        #                   ip_desc.items())
+
+        # return stack_plan
+
+        # stack_desc = self._launch_heat_plan("ferry-app-%s-%s" % (ctype.upper(), cluster_uuid), stack_plan, stack_desc)
+
+        # # Now find all the IP addresses of the various
+        # # machines. 
+        # return self._collect_network_info(stack_desc)
+
+    def alloc(self, cluster_uuid, service_uuid, container_info, ctype, proxy):
+        """
+        Allocate a new cluster. 
+        """
+        # Now take the cluster and create the security group
+        # to expose all the right ports. 
+        sec_group_ports = []
+        internal_ports = []
+        if ctype == "connector": 
+            # Since this is a connector, we need to expose
+            # the public ports. For now, we ignore the host port. 
+            floating_ip = True
+            for c in container_info:
+                for p in c['ports']:
+                    s = str(p).split(":")
+                    if len(s) > 1:
+                        sec_group_ports.append( (s[1], s[1]) )
+                    else:
+                        sec_group_ports.append( (s[0], s[0]) )
         else:
-            ip_plan = { "Resources" : {}}
-            ip_desc = {}
+            if proxy:
+                # If the controller is acting as a proxy, then it has
+                # direct access to the VMs, so the backend shouldn't
+                # get any floating IPs. 
+                floating_ip = False
+            else:
+                # Otherwise, the backend should also get floating IPs
+                # so that the controller can access it. 
+                floating_ip = True
 
-        # Now we need to combine all these plans and
-        # launch the cluster. 
-        stack_plan["Resources"] = dict(sec_group_plan["Resources"].items() + 
-                                       ip_plan["Resources"].items() + 
-                                       stack_plan["Resources"].items())
-        stack_desc = dict(stack_desc.items() + 
-                          sec_group_desc.items() +
-                          ip_desc.items())
-        stack_desc = self._launch_heat_plan("ferry-app-%s-%s" % (ctype.upper(), cluster_uuid), stack_plan, stack_desc)
+            # We need to create a range tuple, so check if 
+            # the exposed port is a range.
+            for p in container_info[0]['exposed']:
+                s = p.split("-")
+                if len(s) == 1:
+                    sec_group_ports.append( (s[0], s[0]) )
+                else:
+                    sec_group_ports.append( (s[0], s[1]) )
 
-        # Now find all the IP addresses of the various
-        # machines. 
-        return self._collect_network_info(stack_desc)
+            # Also see if there are any ports that should be
+            # open within the cluster (but not outside). Typically
+            # used for IPC (where ports may be assigned within a random range). 
+            for p in container_info[0]['internal']:
+                s = p.split("-")
+                if len(s) == 1:
+                    internal_ports.append( (s[0], s[0]) )
+                else:
+                    internal_ports.append( (s[0], s[1]) )
 
-# def main():
-#     fabric = AWSLauncherHeat(sys.argv[1])
-#     fabric.test_neutron()
+        # Tell AWS to allocate the cluster. 
+        resources = self._create_app_stack(cluster_uuid = cluster_uuid, 
+                                           num_instances = len(container_info), 
+                                           security_group_ports = sec_group_ports,
+                                           internal_ports = internal_ports, 
+                                           assign_floating_ip = floating_ip,
+                                           ctype = ctype)
+        
+        logging.warning("STACK: " + str(resources))
 
-# if __name__ == "__main__":
-#     main()
+        # Now we need to ask the cluster to start the 
+        # Docker containers.
+        containers = []
+        mounts = {}
+
+        # if resources:
+        #     # Store the resources cluster ID. 
+        #     self._update_app_db(cluster_uuid, service_uuid, resources)
+
+        #     servers = self._get_servers(resources)
+        #     for i in range(0, len(container_info)):
+        #         # Fetch a server to run the Docker commands. 
+        #         server = servers[i]
+
+        #         # Get the LXC networking options
+        #         lxc_opts, private_ip = self._get_net_info(server, self.subnet, resources)
+
+        #         # Now get an addressable IP address. If we're acting as a proxy within
+        #         # the same cluster, we can just use the private address. Otherwise
+        #         # we'll need to route via the public IP address. 
+        #         if proxy:
+        #             server_ip = private_ip
+        #         else:
+        #             server_ip = self._get_public_ip(server, resources)
+
+        #         # Verify that the user_data processes all started properly
+        #         # and that the docker daemon is actually running. If it is
+        #         # not running, try re-executing. 
+        #         if not self.controller._verify_ferry_server(server_ip):
+        #             self.controller._execute_server_init(server_ip)
+
+        #         # Copy over the public keys, but also verify that it does
+        #         # get copied over properly. 
+        #         self.controller._copy_public_keys(container_info[i], server_ip)
+        #         if self.controller._verify_public_keys(server_ip):
+        #             container, cmounts = self.controller.execute_docker_containers(container_info[i], lxc_opts, private_ip, server_ip)
+                
+        #             if container:
+        #                 mounts = dict(mounts.items() + cmounts.items())
+        #                 containers.append(container)
+        #         else:
+        #             logging.error("could not copy over ssh key!")
+        #             return None
+
+        #     # Check if we need to set the file permissions
+        #     # for the mounted volumes. 
+        #     for c, i in mounts.items():
+        #         for _, v in i['vols']:
+        #             self.controller.cmd([c], 'chown -R %s %s' % (i['user'], v))
+        #     return containers
+        # else:
+        #     # AWS failed to launch the application stack.
+        #     return None
