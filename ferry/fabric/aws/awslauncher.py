4850933483700283bb09f96fa5a58a7f67c2a0d0
James Horey <jlh@opencore.io>
2014-09-23 16:03:00 +0000

    network ids

--- ferry/fabric/aws/awslauncher.py
+++ ferry/fabric/aws/awslauncher.py
@@ -808,9 +808,21 @@ class AWSLauncher(object):
             stack_desc = self._launch_cloudformation("FerryNetwork%s" % cluster_uuid.replace("-", ""), stack_plan, stack_desc)
 
             # Collect the network resources IDs. 
-            vpc_id = stack_desc[vpc_name]
-            data_id = stack_desc[data_subnet_name]
-            manage_id = stack_desc[manage_subnet_name]
+            if vpc_name in stack_desc:
+                vpc_id = stack_desc[vpc_name]
+            else:
+                vpc_id = vpc_name
+
+            if not self.data_subnet:
+                data_id = stack_desc[data_subnet_name]
+            else:
+                data_id = self.data_subnet
+
+            if not self.manage_subnet:
+                manage_id = stack_desc[manage_subnet_name]
+            else:
+                manage_id = self.manage_subnet
+
             return vpc_id, data_id, manage_id
         else:
             return self.vpc_id, self.data_subnet, self.manage_subnet
