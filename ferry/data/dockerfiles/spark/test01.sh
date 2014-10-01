f53059409a4443c067ba346262fd3e6d3d381db4
James Horey <jlh@opencore.io>
2014-10-01 15:07:08 +0000

    update spark examples

--- ferry/data/dockerfiles/spark/test01.sh
+++ ferry/data/dockerfiles/spark/test01.sh
@@ -27,10 +27,10 @@ if [ $1 == "python" ]; then
     fi
 elif [ $1 == "load" ]; then
     MKDIR='hdfs dfs -mkdir -p /spark/data'
-    COPY1='hdfs dfs -copyFromLocal $SPARK_HOME/mllib/data/als/test.data /spark/data/als.data'
-    COPY2='hdfs dfs -copyFromLocal $SPARK_HOME/mllib/data/sample_svm_data.txt /spark/data/svm.txt'
-    COPY3='hdfs dfs -copyFromLocal $SPARK_HOME/mllib/data/ridge-data/lpsa.data /spark/data/lpsa.data'
-    COPY4='hdfs dfs -copyFromLocal $SPARK_HOME/data/kmeans_data.txt /spark/data/kmeans.txt'
+    COPY1='hdfs dfs -copyFromLocal /service/examples/data/als/test.data /spark/data/als.data'
+    COPY2='hdfs dfs -copyFromLocal /service/examples/data/sample_svm_data.txt /spark/data/svm.txt'
+    COPY3='hdfs dfs -copyFromLocal /service/examples/data/ridge-data/lpsa.data /spark/data/lpsa.data'
+    COPY4='hdfs dfs -copyFromLocal /service/examples/data/kmeans_data.txt /spark/data/kmeans.txt'
 
     run_as_ferry "$MKDIR" "Making Spark data directories"
     run_as_ferry "$COPY1" "Upload dataset 1"
@@ -38,5 +38,5 @@ elif [ $1 == "load" ]; then
     run_as_ferry "$COPY3" "Upload dataset 3"
     run_as_ferry "$COPY4" "Upload dataset 4"
 elif [ $1 == "example" ]; then
-    run_as_ferry '$SPARK_HOME/bin/run-example org.apache.spark.examples.SparkPi spark://$BACKEND_COMPUTE_MASTER:7077'
+    run_as_ferry '$SPARK_HOME/bin/spark-submit --class org.apache.spark.examples.SparkPi --master spark://$BACKEND_COMPUTE_MASTER:7077 $SPARK_HOME/lib/spark-examples-1.1.0-hadoop2.4.0.jar'
 fi
